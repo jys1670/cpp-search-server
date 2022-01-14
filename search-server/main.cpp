0ceb065e045ad43d9026141b8d548d7b6060400b
jys1670 <jys1670@hotmail.com>
2022-01-14 13:46:32 +0300

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,28 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 0, j = 0, k = 0, counter = 0;
+    // [0, 1000)
+    while (i < 10) {
+        while (j < 10) {
+            while (k < 10) {
+                if (i == 3 || j == 3 || k == 3) {
+                    ++counter;
+                }
+                ++k;
+            }
+            ++j;
+            k = 0;
+        }
+        ++i;
+        j = 0;
+    }
+    cout << to_string(counter) << endl;
+    return 0;
+}
